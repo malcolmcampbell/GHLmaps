7de7d2343cce7db0ab1b8142884f7f40979972cd
Malcolm H Campbell <malcolm.campbell@canterbury.ac.nz>
2026-02-09 17:40:17 +1300

    Update GHL_MappingGoodsBads.R

--- code/GHL_MappingGoodsBads.R
+++ code/GHL_MappingGoodsBads.R
@@ -8,6 +8,11 @@ pacman::p_load(sf, tmap, tidyverse)
 
 #mapping goods and bads
 
+# VERSION TO DOWNLOAD AND UNZIP BEFORE READING THE FILE
+# temp <- tempfile()
+# download.file("https://www.canterbury.ac.nz/content/dam/uoc-main-site/documents/zip-files/geohealth-laboratory/Environmental_goods_bads_MB2018.zip", temp)
+# GB <-st_read(unz(temp))
+
 setwd("./goods_bads/Environmental_goods_bads_MB2018/")
 
 GB <- st_read("MB2018_exposures_GB.gpkg")
@@ -19,4 +24,5 @@ tm_shape(GB) +
 tm_shape(GB) + 
   tm_polygons(fill="Bads_dec", fill.scale = tm_scale_intervals(n=10, values = "-scico.roma"),
               col=NULL)
+
 # end
